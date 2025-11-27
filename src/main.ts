ed8408aaedc18cf2fedfae3a295e1e55f3f8be39
ElKev118 <kevin.ruda@udea.edu.co>
2025-11-26 19:43:30 -0500

    adding max size 10mb to nats config for large file uploads

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
       transport: Transport.NATS,
       options: {
         servers: envs.natsServers,
+        maxPacketSize: 10 * 1024 * 1024,
         queue: 'payments-service',
       },
     },
